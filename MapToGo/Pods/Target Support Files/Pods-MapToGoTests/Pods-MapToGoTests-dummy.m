974d980d25c17068721ac598c512d18cc14bdaf3
Denys Zhukov <zhukovdenys@gmail.com>
2018-04-13 16:51:04 +0300

    create project, add pod file

---
+++ MapToGo/Pods/Target Support Files/Pods-MapToGoTests/Pods-MapToGoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MapToGoTests : NSObject
+@end
+@implementation PodsDummy_Pods_MapToGoTests
+@end
---
+++ MapToGo/Pods/Target Support Files/Pods-MapToGoUITests/Pods-MapToGoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MapToGoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MapToGoUITests
+@end
